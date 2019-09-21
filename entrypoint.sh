78443f2ac74fbe76103c23873027affde1629a3f
David Pfahler <djpfahler@gmail.com>
2019-09-21 10:00:57 +0200

    fix: check GIT_DEPLOY_KEY

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ remote_repo="git@github.com:${GITHUB_REPOSITORY}.git"
 remote_branch=${BRANCH:-gh-pages}
 
 # check if deploy key is set
-if [ -z ${var+x} ]; then echo "❌ GIT_DEPLOY_KEY not set" && exit 1; else echo "✅deploy key is set"; fi
+if [ -z ${GIT_DEPLOY_KEY+x} ]; then echo "❌ GIT_DEPLOY_KEY not set" && exit 1; else echo "✅deploy key is set"; fi
 
 mkdir /root/.ssh
 echo 'github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' > /root/.ssh/known_hosts
